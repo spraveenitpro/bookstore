d3ca57cdcc1abdd757d7576d94b67e1894601280
Praveen <spraveenitpro@gmail.com>
2023-09-11 23:30:00 -0700

    Daily update

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 export const mongoDBURL =
-	`mongodb+srv://root:root@book-store.vdugryi.mongodb.net/?retryWrites=true&w=majority`
+    'mongodb+srv://root:root@books-collection.3epgbks.mongodb.net/books-collection?retryWrites=true&w=majority';
--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,97 @@ app.get("/", (request, response) => {
 	return response.status(234).send("Welcome to MERN tutorial");
 })
 
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+// Route for saving a book
+
+app.post('/books', async (request, response) => {
+	try {
+		if (
+			!request.body.title ||
+			!request.body.author ||
+			!request.body.publishYear
+		) {
+			return response.status(400).send({ message: "Required field(s) missing" });
+		}
+		const newBook = {
+			title: request.body.title,
+			author: request.body.author,
+			publishYear: request.body.publishYear,
+		}
+
+		const book = await Book.create(newBook);
+		return response.status(201).send(book);
+
+	} catch (error) {
+		console.log(error.message);
+		response.status(500).send({ message: error.message });
+	}
+});
+
+// Route for getting all books
+
+app.get('/books', async (request, response) => {
+	try {
+		const books = await Book.find({});
+		return response.status(200).json({
+			count: books.length,
+			data: books,
+		});
+
+	} catch (error) {
+		console.log(error.message);
+		response.status(500).send({ message: error.message });
+	}
+});
+
+// Route one book by id
+
+app.get('/books/:id', async (request, response) => {
+	try {
+		const { id } = request.params;
+		const book = await Book.findById(id);
+		return response.status(200).json(book);
+
+	} catch (error) {
+		console.log(error.message);
+		response.status(500).send({ message: error.message });
+	}
+});
+
+// Route for updating a book
+
+app.put('/books/:id', async (request, response) => {
+	try {
+		if (!request.body.title ||
+			!request.body.author ||
+			!request.body.publishYear
+		) {
+			return response.status(400).send({ message: "Required field(s) missing" });
+		}
+		const { id } = request.params;
+		const result = await Book.findByIdAndUpdate(id, request.body);
+		if (!result) {
+			return response.status(404).send({ message: "Book not found" });
+		}
+		return response.status(200).send({ message: "Book updated successfully" });
+
+
+	} catch (error) {
+		console.log(error.message);
+		response.status(500).send({ message: error.message });
+	}
+
 })
+
+
+
+mongoose
+	.connect(mongoDBURL)
+	.then(() => {
+		console.log("App Connected to MongoDB");
+		app.listen(PORT, () => {
+			console.log(`Server is running on port ${PORT}`);
+		})
+	})
+	.catch((error) => {
+		console.log(error);
+	});
